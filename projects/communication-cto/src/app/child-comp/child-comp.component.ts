1be86998ab799fa8bce15af397891fe011ebe784
SwatiMahapatra <swatim9919@gmail.com>
2024-01-20 09:12:04 +0530

    first commit

---
+++ projects/communication-cto/src/app/child-comp/child-comp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-comp',
+  templateUrl: './child-comp.component.html',
+  styleUrl: './child-comp.component.css'
+})
+export class ChildCompComponent {
+
+}
